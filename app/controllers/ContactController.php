a69cad1ea745071b2a7412bcd1b9da563a6b59a1
Chonabot Intakeha <chon@woorus.com>
2015-02-09 02:45:18 -0800

    Update ContactController.php validate function for "required"

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -10,7 +10,7 @@ class ContactController extends BaseController {
 
 		//Validation rules
 		$rules = array (
-			'name' => 'required|required',
+			'name' => 'required',
 			'email' => 'required|email',
 			'message' => 'required'
 		);
